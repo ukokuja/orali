b60ef42c83f360f63fb68d01cb0e98d2a5af0626
Kevin Kujawski <kevinkuja@gmail.com>
2015-03-30 23:44:35 -0300

    TODO

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,16 +1,35 @@
 angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope) {})
+.controller('MapCtrl', function($scope, $ionicLoading, $compile) {
+	
+  $scope.initialize = function() {
+    var myLatlng = new google.maps.LatLng(43.07493,-89.381388);
+    
+    var mapOptions = {
+      center: myLatlng,
+      zoom: 16,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(document.getElementById("map"),
+        mapOptions);
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
+
+    var marker = new google.maps.Marker({
+      position: myLatlng,
+      map: map,
+      title: 'Uluru (Ayers Rock)'
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+      infowindow.open(map,marker);
+    });
+
+    $scope.map = map;
   }
-})
+  //google.maps.event.addDomListener(window, 'load', initialize);
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
+  
 })
 
 .controller('AccountCtrl', function($scope) {
